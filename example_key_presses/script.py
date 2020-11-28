bc258fdc1686258c0f8273e2c5698b84f09eef3f
johnand4 <johnand4@oregonstate.edu>
2020-11-28 07:03:14 -0800

    Reworked script and created DO files

--- example_key_presses/script.py
+++ example_key_presses/script.py
@@ -2,92 +2,94 @@
 import bpy
 import os
 import math
-import struct
-import ctypes
 
-# Convert a float into a string representing the IEE 745 standard for a 32-bit float
-def to_binary(num):
-    return ''.join(bin(ord(str(c)[0])).replace('0b', '').rjust(8, '0') for c in struct.pack('!f', num))
+#---------------------------------------------------------------------
+# This function takes a list of floats from an oscilloscope 
+# representing voltage and generates a force statement 
+# replicating the voltages.
+# Params:
+#   list - float array holding voltages
+#   name - string holding name of variable to force 
+#   vl   - the minimum voltage allowed
+#   vh   - the maximum voltage allowed
+#--------------------------------------------------------------------- 
+def gen_force(list, name, vl, vh):
+    force = "force " + name + " "
+    prev = -20  # Previous HIGH/LOW voltage
+    i = 0
+    for curr in list:
+        if curr - prev >= vh - vl:   # Changes to LOW signal
+            force = force + "1 @ " + "{0}".format(i) + "ps, "
+            prev = curr
+        elif curr - prev <= vl - vh: # Changes to HIGH signal
+            force = force + "0 @ " + "{0}".format(i) + "ps, "
+            prev = curr
+        i = i + 1
+    force = force[:-2] # Remove last ', ' using slicing
+    return force
 
-def convert_to_do_file(name):
-    # Get filepath to this Blender file and find the file to write to
-    blend_file_path = bpy.data.filepath
-    directory = os.path.dirname(blend_file_path)
-    target_file = os.path.join(directory, 'PS2Keyboard')
-    target_file = os.path.join(target_file, name + '.csv')
-
-    # Open the file for reading and store each line separately 
-    f = open(target_file, "r")
-    Lines = f.readlines()
-
+#--------------------------------------------------------------------
+# This function converts a given CSV file into a string representing
+# the contents of a DO file ready for simulation in ModelSim
+# Params:
+#   name - the filename of the CSV file to be read
+#--------------------------------------------------------------------
+def csv_to_do(name):
     # Place all values into these lists
     time = []
     ch1 = []
     ch2 = []
-    i = 0
-    for line in Lines:
-        if line.strip() == ",,":
-            break
-        elif i > 14:
-            l = line.strip().split(',')
-            #print(format("{:f}, {:f}".format(float(l[0].split("e")[0]), float(l[0].split("e")[1]))))
-            
-            t = float(l[0].split("e")[0]) * pow(10, float(l[0].split("e")[1]))
-            #values = [t, float(l[1].replace(" ","")), float(l[2].replace(" ",""))]
-            time.append(t)
-            ch1.append(float(l[1].replace(" ","")))
-            ch2.append(float(l[2].replace(" ","")))
-        else:
-            i = i + 1
+    
+    # Find the filepath for the file to open
+    target_file = os.path.join(directory, 'PS2Keyboard')
+    target_file = os.path.join(target_file, name)
 
-    #i = 0;
-    #for t in time: # Print out the values to see if we have read them correctly
-    #    print("{:f}, {:f}, {:f}".format(t, ch1[i], ch2[i]))
-    #    i = i + 1
+    # Open the file for reading and store each line separately 
+    f = open(target_file, "r")
+    Lines = f.readlines()
+    
+    # Parse each line in CSV and store floats in proper lists
+    for line in Lines[14:-1]: # Ignore first 14 and last 1 lines
+        l = line.strip().split(',')
+        t = float(l[0].split("e")[0]) * pow(10, float(l[0].split("e")[1]))
+        time.append(t)
+        ch1.append(float(l[1]))
+        ch2.append(float(l[2]))
         
-    f.close() # Close the file, we are done reading
+    f.close() # We are done reading
+    
+    # Generate the force statements with LOW=0.6 and HIGH=3.3
+    force_ch1 = gen_force(ch1, name + "_ch1", 0.6, 3.3)
+    force_ch2 = gen_force(ch2, name + "_ch2", 0.6, 3.3)
+    
+    return force_ch1 + "\n" + force_ch2 # Return file contents
 
+#-----------------------------------------------------------------#
+#------------------------Function Calls---------------------------#
+
+# List holding filenames of all CSV files
+files = ["a", "a_hold","d","e","f","g","r","s","t","w","water"]
+
+# Get filepath to this Blender file and find the file to write to
+blend_file_path = bpy.data.filepath
+directory = os.path.dirname(blend_file_path)
+
+# Find the file we will be writing the do file to
+target_file = os.path.join(directory, 'PS2Keyboard')
+target_file = os.path.join(target_file, 'force.do')
+
+k = open(target_file, "w") # Open the combined DO file
+
+for file in files: # Create DO file for each CSV file
     # Find the file we will be writing the do file to
     target_file = os.path.join(directory, 'PS2Keyboard')
-    target_file = os.path.join(target_file, name + '.do')
-
-    # Open the do file
-    k = open(target_file, "w")
-
-    # Write a new change only if the change is within the range (0.6, 3.3)
-    force_ch1 = "force ch1 "
-    force_ch2 = "force ch2 "
-    i = 0
-    current_ch1 = 20
-    current_ch2 = 20
-    previous_ch1 = -20
-    previous_ch2 = -20
-    for t in time:
-        current_ch1 = ch1[i]
-        current_ch2 = ch2[i]
-        if abs(current_ch1 - previous_ch1) >= 3.3-0.6:
-            force_ch1 = force_ch1 + to_binary(current_ch1) + " @ " + to_binary(t) + " "
-            previous_ch1 = current_ch1
-        if abs(current_ch2 - previous_ch2) >= 3.3-0.6:
-            force_ch2 = force_ch2 + to_binary(current_ch2) + " @ " + to_binary(t) + " "
-            previous_ch2 = current_ch2
-        i = i + 1
-
-    k.write(force_ch1)
-    k.write("\n")
-    k.write(force_ch2)
-    k.close()
+    target_file = os.path.join(target_file, file + '.do')
     
+    data = csv_to_do(file + ".csv") # Get file data
     
-    
-convert_to_do_file("a")
-convert_to_do_file("a_hold")
-convert_to_do_file("d")
-convert_to_do_file("e")
-convert_to_do_file("f")
-convert_to_do_file("g")
-convert_to_do_file("r")
-convert_to_do_file("s")
-convert_to_do_file("t")
-convert_to_do_file("w")
-convert_to_do_file("water")
+    f = open(target_file, "w") # Open the DO file for writing
+    f.write(data)              # Write data to specific file
+    k.write(data + "\n")       # Write data to combined file 
+    f.close()                  # Close file
+
+k.close() # Close combined file, we are done
